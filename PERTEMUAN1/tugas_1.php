0d3b7f615574051e0d783ea07ca03f4459a1a435
Gunawan Raharjo <36659860+gunawan69@users.noreply.github.com>
2018-02-20 19:24:02 +0700

    Add files via upload

---
+++ PERTEMUAN1/tugas_1.php
@@ -0,0 +1,17 @@
+<?php
+echo "<marquee><h1><center>TUGAS PEMROGRAMAN WEB 1 </marquee></h1></center>";
+$nim = "16090035";
+$nama = 'Gunawan Raharjo';
+$kelas= "3D";
+$alamat= "Tegal";
+$matakul="PEMROGRAMAN WEB 1";
+$semester="4";
+
+echo "<center>NAMA : " . $nama . "<br>";
+echo "NIM : $nim <br>";
+echo "KELAS : $kelas <br>";
+echo "SEMESTER : $semester <br>";
+echo "ALAMAT : $alamat <br>";
+echo "MATA KULIHAN: $matakul <br>";
+
+?>
